6a31a8917be618fdca48c3203aad6c067d64b26e
Javier <javierz87@gmail.com>
2023-11-04 14:43:13 -0400

    search_by_text implemented

--- js/controller.js
+++ js/controller.js
@@ -197,6 +197,49 @@ function renderProductosByCategoryPrice() {
     });
 }
 
+function search_products(){
+    let result;
+    let search = document.getElementById('searcher');
+    result = products.filter(products => products.name.toLowerCase().includes(search.value.toLowerCase()))
+    document.getElementById('InitialContent').innerHTML = '';
+
+    if(search.value != '')
+    for(let i=0; i< result.length; i++){
+        document.getElementById('InitialContent').innerHTML += 
+        ` <div class="col-md-6 col-lg-3 ftco-animate fadeInUp ftco-animated">
+        <div class="product">
+            <a href="#" class="img-prod"><img class="img-fluid" src="${result[i].imageURL}" alt="Colorlib Template">
+                <div class="overlay"></div>
+            </a>
+            <div class="text py-3 pb-4 px-3 text-center">
+                <h3><a href="#">${result[i].name}</a></h3>
+                <div class="d-flex">
+                    <div class="pricing">
+                        <p class="price"><span>$${result[i].price.toFixed(2)}</span></p>
+                    </div>
+                </div>
+                <div class="bottom-area d-flex px-3">
+                    <div class="m-auto d-flex">
+                        <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-center">
+                            <span><i class="ion-ios-menu"></i></span>
+                        </a>
+                        <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
+                            <span><i class="ion-ios-cart"></i></span>
+                        </a>
+                        <a href="#" class="heart d-flex justify-content-center align-items-center ">
+                            <span><i class="ion-ios-heart"></i></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>`
+    }
+
+
+
+}
+
 const rangeInput = document.getElementById('rangeInput');
 const rangeValue = document.getElementById('rangeValue');
 rangeInput.addEventListener('input', showValue)
@@ -204,3 +247,4 @@ rangeInput.addEventListener('input', showValue)
 function showValue() {
     rangeValue.textContent = rangeInput.value;
 }
+renderProductos()
