77a1e55251a7ead53e7e97fcdce9a62f8fdf6a14
Javier <javierz87@gmail.com>
2023-11-23 20:10:54 -0500

    Controller change to jquery

--- js/controller.js
+++ js/controller.js
@@ -8,239 +8,108 @@ let products = JSON.parse(localStorage.getItem('products'));
 
 
 let valueCategory = ' ';
+
 function selectOption(id, menuOption) {
-    document.getElementById('InitialContent').style.display = 'none';
-    document.getElementById(id).style.display = 'flex';
-    document.querySelectorAll('.menu-option').forEach(etiqueta => {
-        etiqueta.classList.remove('active')
-    })
-
-    menuOption.classList.add('active');
-
-
+    $('#InitialContent').hide();
+    $('#' + id).show();
+    $('.menu-option').removeClass('active');
+    $(menuOption).addClass('active');
 }
-
 
 function renderProductos() {
-
-    valueCategory = '';
-    // let productosPorNegocio = productos.filter(producto => producto.negocio == idNegocio);
-
-
-    document.getElementById('InitialContent').innerHTML = '';
-
-    for (let i = 0; i < products.length; i++) {
-
-        document.getElementById('InitialContent').innerHTML +=
-            `
-
-    <div  class="col-md-6 col-lg-3 ftco-animate fadeInUp ftco-animated">
-        <div class="product">
-            <a href="product-single.html?id=${products[i].id}" class="img-prod"><img class="img-fluid" src="${products[i].imageURL}" alt="Colorlib Template">
-                <div class="overlay"></div>
-            </a>
-            <div class="text py-3 pb-4 px-3 text-center">
-                <h3><a href="#">${products[i].name}</a></h3>
-                <div class="d-flex">
-                    <div class="pricing">
-                        <p class="price"><span>$${products[i].price.toFixed(2)}</span></p>
-                    </div>
-                </div>
-                <div class="bottom-area d-flex px-3">
-                    <div class="m-auto d-flex">
-                        <a href="#" class="buy-now d-flex justify-content-center align-items-center text-center data-product-id = ${products[i].id}">
-                            <span><i class="ion-ios-menu"></i></span>
-                        </a>
-                        <a href="#" class="add-to-cart d-flex justify-content-center align-items-center mx-1" data-product-id = "${products[i].id}">
-                            <span><i class="ion-ios-cart"></i></span>
-                        </a>
-                        <a href="#" class="heart d-flex justify-content-center align-items-center ">
-                            <span><i class="ion-ios-heart"></i></span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-     </div>
-
-   `
-
-    }
-
-    document.getElementById('InitialContent').style.display = 'flex';
-
-    document.querySelectorAll('.menu-option').forEach(etiqueta => {
-        etiqueta.classList.remove('active')
-    });
-
-
+    appendProductHTML(products);
 }
+
 function renderProductosByCategory(category) {
-
     valueCategory = category;
-    let productsByCategory = products.filter(products => products.category == category);
 
-console.log("sdfsfsdf"+productsByCategory)
-    document.getElementById('InitialContent').innerHTML = '';
-
-console.log(productsByCategory)
-    for (let i = 0; i < productsByCategory.length; i++) {
-
-        document.getElementById('InitialContent').innerHTML +=
-            `
-
-    <div class="col-md-6 col-lg-3 ftco-animate fadeInUp ftco-animated">
-        <div class="product">
-            <a href="product-single.html?id=${productsByCategory[i].id}" class="img-prod"><img class="img-fluid" src="${productsByCategory[i].imageURL}" alt="Colorlib Template">
-                <div class="overlay"></div>
-            </a>
-            <div class="text py-3 pb-4 px-3 text-center">
-                <h3><a href="#">${productsByCategory[i].name}</a></h3>
-                <div class="d-flex">
-                    <div class="pricing">
-                        <p class="price"><span>$${productsByCategory[i].price.toFixed(2)}</span></p>
-                    </div>
-                </div>
-                <div class="bottom-area d-flex px-3">
-                    <div class="m-auto d-flex">
-                        <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-center">
-                            <span><i class="ion-ios-menu"></i></span>
-                        </a>
-                        <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
-                            <span><i class="ion-ios-cart"></i></span>
-                        </a>
-                        <a href="#" class="heart d-flex justify-content-center align-items-center ">
-                            <span><i class="ion-ios-heart"></i></span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-     </div>
-
-   `
-
-    }
-
-
-
-
-    document.getElementById('InitialContent').style.display = 'flex';
-
-    document.querySelectorAll('.menu-option').forEach(etiqueta => {
-        etiqueta.classList.remove('active')
-    });
-
-
+    let productsByCategory = products.filter(p => p.category === category);
+    console.log("sdfsfsdf"+productsByCategory)
+    appendProductHTML(productsByCategory);
 }
-
-
 
 function renderProductosByCategoryPrice() {
     const category = valueCategory;
-    const price = document.getElementById('rangeValue').textContent;
-    console.log(price);
-    if (valueCategory == '') {
-        var renderProductosByCategoryPrice = products.filter(products => (products.price <= price));
+    const priceValue = $('#rangeValue').text();
+    const price = parseFloat(priceValue);
+    console.log('Price:', price);
+
+    var filteredProducts;
+    if (valueCategory === ' ') {
+        filteredProducts = products.filter(p => p.price <= price);
     } else {
-        var renderProductosByCategoryPrice = products.filter(products => (products.category == category) && (products.price <= price));
+        filteredProducts = products.filter(p => p.category === category && parseFloat(p.price) <= price);
     }
-    console.log(renderProductosByCategoryPrice);
-    document.getElementById('InitialContent').innerHTML = '';
+    console.log('Filtered Products:', filteredProducts);
+
+    appendProductHTML(filteredProducts);
+}
 
 
-    for (let i = 0; i < renderProductosByCategoryPrice.length; i++) {
+function search_products() {
+    let searchValue = $('#searcher').val().toLowerCase();
+    let result = products.filter(p => p.name.toLowerCase().includes(searchValue));
 
-        document.getElementById('InitialContent').innerHTML +=
-            `
+    if (searchValue !== ' ') {
+        appendProductHTML(result);
+    }
+}
 
-    <div class="col-md-6 col-lg-3 ftco-animate fadeInUp ftco-animated">
-        <div class="product">
-            <a href="product-single.html?id=${renderProductosByCategoryPrice[i].id}" class="img-prod"><img class="img-fluid" src="${renderProductosByCategoryPrice[i].imageURL}" alt="Colorlib Template">
-                <div class="overlay"></div>
-            </a>
-            <div class="text py-3 pb-4 px-3 text-center">
-                <h3><a href="#">${renderProductosByCategoryPrice[i].name}</a></h3>
-                <div class="d-flex">
-                    <div class="pricing">
-                        <p class="price"><span>$${renderProductosByCategoryPrice[i].price.toFixed(2)}</span></p>
-                    </div>
-                </div>
-                <div class="bottom-area d-flex px-3">
-                    <div class="m-auto d-flex">
-                        <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-center">
-                            <span><i class="ion-ios-menu"></i></span>
-                        </a>
-                        <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
-                            <span><i class="ion-ios-cart"></i></span>
-                        </a>
-                        <a href="#" class="heart d-flex justify-content-center align-items-center ">
-                            <span><i class="ion-ios-heart"></i></span>
-                        </a>
+
+
+function appendProductHTML(productsToRender) {
+    $('#InitialContent').empty().css('display', 'flex');
+  
+   
+    $.each(productsToRender, function(index, product) {
+        let priceTag = ' ' ;
+        let discountTag=' ' ;
+        
+        if (product.discount>0){
+            let discountedPrice=product.price - (product.price*product.discount/100);
+            console.log("discount" + discountedPrice);
+            priceTag=`<p class="price"><span class="mr-2 price-dc">$${product.price.toFixed(2)}</span><span class="price-sale">$${discountedPrice.toFixed(2)}</span></p>`;
+            discountTag=`<span class="status">${product.discount}%</span>`;
+        }else { 
+            priceTag=`<p class="price"><span>$${product.price.toFixed(2)}</span></p>`;
+            discountTag=' ' ;
+        }
+        $('#InitialContent').append(`
+            <div class="col-md-6 col-lg-3 ftco-animate fadeInUp ftco-animated">
+                <div class="product">
+                    <a href="product-single.html?id=${product.id}" class="img-prod"><img class="img-fluid" src="${product.imageURL}" alt="Colorlib Template">
+                       ${discountTag}
+                        <div class="overlay"></div>
+                    </a>
+                    <div class="text py-3 pb-4 px-3 text-center">
+                        <h3><a href="#">${product.name}</a></h3>
+                        <div class="d-flex">
+                            <div class="pricing">
+                               ${priceTag}
+                            </div>
+                        </div>
+                        <div class="bottom-area d-flex px-3">
+                            <div class="m-auto d-flex">
+                                <a href="#" class="buy-now d-flex justify-content-center align-items-center text-center" data-product-id="${product.id}">
+                                    <span><i class="ion-ios-menu"></i></span>
+                                </a>
+                                <a href="#" class="add-to-cart d-flex justify-content-center align-items-center mx-1" data-product-id="${product.id}">
+                                    <span><i class="ion-ios-cart"></i></span>
+                                </a>
+                                <a href="#" class="heart d-flex justify-content-center align-items-center">
+                                    <span><i class="ion-ios-heart"></i></span>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-     </div>
-
-   `
-
-    }
-
-
-
-
-    document.getElementById('InitialContent').style.display = 'flex';
-
-    document.querySelectorAll('.menu-option').forEach(etiqueta => {
-        etiqueta.classList.remove('active')
+        `);
     });
+
+    $('.menu-option').removeClass('active');
 }
 
-function search_products(){
-    let result;
-    let search = document.getElementById('searcher');
-    result = products.filter(products => products.name.toLowerCase().includes(search.value.toLowerCase()))
-    document.getElementById('InitialContent').innerHTML = '';
-
-
-    if(search.value != ' ')
-    for(let i=0; i< result.length; i++){
-
-        document.getElementById('InitialContent').innerHTML += 
-        ` <div class="col-md-6 col-lg-3 ftco-animate fadeInUp ftco-animated">
-        <div class="product">
-            <a href="product-single.html?id=${result[i].id}" class="img-prod"><img class="img-fluid" src="${result[i].imageURL}" alt="Colorlib Template">
-                <div class="overlay"></div>
-            </a>
-            <div class="text py-3 pb-4 px-3 text-center">
-                <h3><a href="#">${result[i].name}</a></h3>
-                <div class="d-flex">
-                    <div class="pricing">
-                        <p class="price"><span>$${result[i].price.toFixed(2)}</span></p>
-                    </div>
-                </div>
-                <div class="bottom-area d-flex px-3">
-                    <div class="m-auto d-flex">
-                        <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-center">
-                            <span><i class="ion-ios-menu"></i></span>
-                        </a>
-                        <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
-                            <span><i class="ion-ios-cart"></i></span>
-                        </a>
-                        <a href="#" class="heart d-flex justify-content-center align-items-center ">
-                            <span><i class="ion-ios-heart"></i></span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-     </div>`
-    }
-
-
-
-}
 
 const rangeInput = document.getElementById('rangeInput');
 const rangeValue = document.getElementById('rangeValue');
@@ -249,4 +118,7 @@ rangeInput.addEventListener('input', showValue)
 function showValue() {
     rangeValue.textContent = rangeInput.value;
 }
-renderProductos()
+$(document).ready(function() {
+    renderProductos();
+});
+
