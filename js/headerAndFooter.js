1732be1f33b15ce4ae53e1d54c70414cad81dbb3
Mike <fuchun297@gmail.com>
2023-11-24 09:11:04 -0500

    change the footer menu

---
+++ js/headerAndFooter.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    document.getElementById('footer').innerHTML +=' <footer class="ftco-footer ftco-section">\n' +
+        '      <div class="container">\n' +
+        '      \t<div class="row">\n' +
+        '      \t\t<div class="mouse">\n' +
+        '\t\t\t\t\t\t<a href="#" class="mouse-icon">\n' +
+        '\t\t\t\t\t\t\t<div class="mouse-wheel"><span class="ion-ios-arrow-up"></span></div>\n' +
+        '\t\t\t\t\t\t</a>\n' +
+        '\t\t\t\t\t</div>\n' +
+        '      \t</div>\n' +
+        '        <div class="row mb-5">\n' +
+        '          <div class="col-md">\n' +
+        '            <div class="ftco-footer-widget mb-4">\n' +
+        '              <h2 class="ftco-heading-2">Vegefoods</h2>\n' +
+        '              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>\n' +
+        '              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">\n' +
+        '                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>\n' +
+        '                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>\n' +
+        '                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>\n' +
+        '              </ul>\n' +
+        '            </div>\n' +
+        '          </div>\n' +
+        '          <div class="col-md">\n' +
+        '            <div class="ftco-footer-widget mb-4 ml-md-5">\n' +
+        '              <h2 class="ftco-heading-2">Menu</h2>\n' +
+        '              <ul class="list-unstyled">\n' +
+        '                <li><a href="shop.html" class="py-2 d-block">Shop</a></li>\n' +
+        '                <li><a href="about.html" class="py-2 d-block">About</a></li>\n' +
+        '                <li><a href="wishlist.html" class="py-2 d-block">Wishlist</a></li>\n' +
+        '                <li><a href="contact.html" class="py-2 d-block">Contact Us</a></li>\n' +
+        '                <li><a href="login.html" class="py-2 d-block">Login</a></li>\n' +
+        '              </ul>\n' +
+        '            </div>\n' +
+        '          </div>\n' +
+        '          <div class="col-md">\n' +
+        '            <div class="ftco-footer-widget mb-4">\n' +
+        '            \t<h2 class="ftco-heading-2">Have a Questions?</h2>\n' +
+        '            \t<div class="block-23 mb-3">\n' +
+        '\t              <ul>\n' +
+        '\t                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>\n' +
+        '\t                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>\n' +
+        '\t                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>\n' +
+        '\t              </ul>\n' +
+        '\t            </div>\n' +
+        '            </div>\n' +
+        '          </div>\n' +
+        '        </div>\n' +
+        '        <div class="row">\n' +
+        '          <div class="col-md-12 text-center">\n' +
+        '\n' +
+        '            <p><!-- Link back to Colorlib can\'t be removed. Template is licensed under CC BY 3.0. -->\n' +
+        '\t\t\t\t\t\t  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>\n' +
+        '\t\t\t\t\t\t  <!-- Link back to Colorlib can\'t be removed. Template is licensed under CC BY 3.0. -->\n' +
+        '\t\t\t\t\t\t</p>\n' +
+        '          </div>\n' +
+        '        </div>\n' +
+        '      </div>\n' +
+        '    </footer>'
+})
